786f2f6f14e4379764a96ee8c5960e73dc5fcf3f
Mohit-Balachander <mohitbalachander@gmail.com>
2024-09-09 20:44:24 +0530

    JS program

---
+++ 4..JavaScript/1.GettingStarted/1.FirstJsCode/script.js
@@ -0,0 +1,2 @@
+// External JS with 'async' attribute
+console.log('This is my first JS code');
---
+++ 4..JavaScript/2.Variable/script.js
@@ -0,0 +1,26 @@
+// Keywords: 'var', 'let`, `const'.
+// - var: Function-scoped, redeclarable, updatable.
+// let': Block-scoped, updatable, not redeclarable.
+// -const: Block-scoped, not updatable, not redeclarable.
+// When to use?
+// - var: Older code, or when function-scoping is needed.
+// - let: Variable value changes.
+// - const: Variable value remains constant. 
+
+//! Use 'var' to declare a variable.
+//! Task : Storing a person's first name
+
+//! var firstName = 'John';
+//! console.log(firstName);
+
+//? Use 'let' to declare a variable.
+//? Task : Storing a person's first name
+
+//? let firstName = 'John';
+//? console.log(firstName);
+
+//* Use 'const' to declare a variable.
+//* Task : Storing a person's first name
+
+const firstName = 'John';
+console.log(firstName);
